4bb7baaf63990a86e0943f410213890a12ef6148
[hamidovdev] <[muhammadrasulcyber@gmail.com]>
2024-03-01 19:20:20 +0500

    mixin va showModalSnackBar degan widget qo'shdim o'zgaruvchilar nomini ham o'zgartirdim

--- lib/features/create/presentation/pages/create_income.dart
+++ lib/features/create/presentation/pages/create_income.dart
@@ -9,6 +9,8 @@ import 'package:productive/core/create_status/income_status.dart';
 import 'package:productive/core/extensions/extensions.dart';
 import 'package:productive/features/create/data/model/income_model.dart';
 import 'package:productive/features/create/presentation/bloc/income_bloc.dart';
+import 'package:productive/features/create/presentation/mixin/create_income_mixin.dart';
+import 'package:productive/features/create/presentation/widgets/show_snackbar.dart';
 import 'package:productive/features/create/presentation/widgets/w_text_field.dart';
 
 class CreateIncomeScreen extends StatefulWidget {
@@ -17,146 +19,12 @@ class CreateIncomeScreen extends StatefulWidget {
   @override
   State<CreateIncomeScreen> createState() => _CreateIncomeScreenState();
 }
-
-class _CreateIncomeScreenState extends State<CreateIncomeScreen> {
-  TextEditingController? title_controller;
-  int usd = 0;
-  TextEditingController? note_controller;
-
+class _CreateIncomeScreenState extends State<CreateIncomeScreen> with CreateIncomeMixin{
   @override
   void initState() {
-    title_controller = TextEditingController();
-    note_controller = TextEditingController();
+    titleController = TextEditingController();
+    noteController = TextEditingController();
     super.initState();
-  }
-
-  String getSvg(IncomePrio prio) {
-    switch (prio) {
-      case IncomePrio.salary:
-        return context.icons.moneyFirst;
-      case IncomePrio.expense:
-        return context.icons.moneySecond;
-    }
-  }
-
-  Color getColor(IncomePrio prio) {
-    switch (prio) {
-      case IncomePrio.expense:
-        return context.colors.addButtonLinear1;
-      case IncomePrio.salary:
-        return context.colors.meetingColor;
-    }
-  }
-
-  void showBottomSheet(BuildContext context, IncomeModel model) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          padding: const EdgeInsets.only(top: 8, bottom: 16),
-          width: double.maxFinite,
-          height: MediaQuery.of(context).size.height / 100 * 27.8,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(24),
-              topRight: Radius.circular(24),
-            ),
-            color: context.colors.bottomSheetBgColor,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const SizedBox(),
-                    Container(
-                      width: 80,
-                      height: 4,
-                      decoration: BoxDecoration(
-                          color:
-                              context.colors.bottomSheetDate.withOpacity(0.4),
-                          borderRadius: BorderRadius.circular(24)),
-                    ),
-                    GestureDetector(child: SvgPicture.asset(context.icons.exit),),
-                  ],
-                ),
-              ),
-              const Gap(24),
-              Divider(
-                color: context.colors.dividerBgColor,
-              ),
-              GestureDetector(
-                onTap: (){},
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: context.colors.meetingColor,
-                        ),
-                        padding: EdgeInsets.all(9),
-                        child: SvgPicture.asset(
-                          context.icons.moneyFirst,
-                        ),
-                      ),
-                      const Gap(8),
-                      Text(
-                        context.localization.salary,
-                        style: context.style.fontSize20Weight500,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Divider(
-                color: context.colors.dividerBgColor,
-              ),
-              GestureDetector(
-                onTap: (){},
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Stack(
-
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child:
-                            Row(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: context.colors.addButtonLinear1,
-                                  ),
-                                  padding: EdgeInsets.all(12),
-                                  child: SvgPicture.asset(
-                                    context.icons.plus,
-                                  ),
-                                ),
-                                const Gap(8),
-                                Text(
-                                  context.localization.salary,
-                                  style: context.style.fontSize20Weight500,
-                                ),
-                              ],
-                            ),
-
-                      ),
-                      SvgPicture.asset(context.icons.premium),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
 
   @override
@@ -196,7 +64,7 @@ class _CreateIncomeScreenState extends State<CreateIncomeScreen> {
                             children: [
                               GestureDetector(
                                 onTap: () {
-                                  showBottomSheet(context, state.incomeModel);
+                                  showBottomSheetWidget(context, state.incomeModel);
                                 },
                                 child: Container(
                                   decoration: BoxDecoration(
@@ -318,11 +186,10 @@ class _CreateIncomeScreenState extends State<CreateIncomeScreen> {
       ),
     );
   }
-
   @override
   void dispose() {
-    title_controller!.dispose();
-    note_controller!.dispose();
+    titleController!.dispose();
+    noteController!.dispose();
     super.dispose();
   }
 }
