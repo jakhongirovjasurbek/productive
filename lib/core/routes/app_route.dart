0435d6dec9aa9c87e3ea9b00281f6d4e1153bb82
Dovudhon <dovudxon.2006.uz@gmail.com>
2024-03-05 21:19:30 +0500

    problems are fixed

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:productive/core/functions/app_functions.dart';
 import 'package:productive/core/pages/onboarding.dart';
+import 'package:productive/core/pages/splash.dart';
 import 'package:productive/features/authentication/presentation/pages/create_new_password.dart';
 import 'package:productive/core/route_names/app_route_name.dart';
 import 'package:productive/features/home/home.dart';
@@ -11,7 +12,7 @@ class AppRoute {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRouteNames.splash:
-        return fade(const StatisticsScreen());
+        return fade(const SplashPage());
       case AppRouteNames.home:
         return fade(const HomePage());
       case AppRouteNames.onboarding:
