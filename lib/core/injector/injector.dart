de0125667d529ff2e90de845037c8032fddf2942
JahongirToxtasinov <gpflutter@gmail.com>
2024-03-14 20:06:35 +0500

    auth status done

--- lib/core/injector/injector.dart
+++ lib/core/injector/injector.dart
@@ -3,11 +3,14 @@ import 'package:productive/features/calendar/data/data_sourse/data_source.dart';
 import 'package:productive/features/calendar/data/repository/repository.dart';
 import 'package:productive/features/calendar/domain/repository/repository.dart';
 import 'package:productive/features/calendar/domain/usecase/usecase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final sl = GetIt.instance;
 
 Future<void> getItInjector() async {
   calendarUseCase();
+  sl.registerSingletonAsync(
+          () async => await SharedPreferences.getInstance());
 }
 
 calendarUseCase() {
