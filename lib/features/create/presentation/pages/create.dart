38f5d24fa0ac92997bf7351ef64184fee71d56b6
MUHAMMADAMIN_JO'RAYEV <136828822+Muhammadamins@users.noreply.github.com>
2024-03-18 18:08:58 +0500

    create screen USER INTERFACE bajarildi

--- lib/features/create/presentation/pages/create.dart
+++ lib/features/create/presentation/pages/create.dart
@@ -17,161 +17,143 @@ class _CreateItemsState extends State<CreateItems> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {},
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: context.colors.createhomeitem,
-                            child: SvgPicture.asset(context.icons.money),
-                          ),
-                          const Gap(8),
-                          DefaultTextStyle(
-                            style: const TextStyle(
-                                decoration: TextDecoration.none),
-                            child: Text(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: context.colors.createhomeitem,
+                              child: SvgPicture.asset(context.icons.money),
+                            ),
+                            const Gap(8),
+                            Text(
                               context.localization.income,
                               style: context.style.fontSize14Weight500.copyWith(
                                 color: context.colors.createhomeitemwhite,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {},
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: context.colors.createhomeitem,
-                            child: SvgPicture.asset(AppIcons.expense),
-                          ),
-                          const Gap(8),
-                          DefaultTextStyle(
-                            style: const TextStyle(
-                                decoration: TextDecoration.none),
-                            child: Text(context.localization.expense,
-                                style:
-                                    context.style.fontSize14Weight500.copyWith(
-                                  color: context.colors.createhomeitemwhite,
-                                )),
-                          ),
-                        ],
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: context.colors.createhomeitem,
-                            child: SvgPicture.asset(context.icons.taskhome),
-                          ),
-                          const Gap(8),
-                          DefaultTextStyle(
-                            style: const TextStyle(
-                                decoration: TextDecoration.none),
-                            child: Text(context.localization.tasks,
-                                style:
-                                    context.style.fontSize14Weight500.copyWith(
-                                  color: context.colors.createhomeitemwhite,
-                                )),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                const Gap(61),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {},
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: context.colors.createhomeitem,
-                            child: SvgPicture.asset(context.icons.note),
-                          ),
-                          const Gap(8),
-                          DefaultTextStyle(
-                            style: const TextStyle(
-                                decoration: TextDecoration.none),
-                            child: Text(context.localization.notes,
-                                style:
-                                    context.style.fontSize14Weight500.copyWith(
-                                  color: context.colors.createhomeitemwhite,
-                                )),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor:
-                              context.colors.createhomeitem.withOpacity(0.0),
+                          ],
                         ),
-                        const Gap(8),
-                        Text("",
-                            style: context.style.fontSize14Weight500.copyWith(
-                              color: context.colors.createhomeitemwhite,
-                            )),
-                      ],
-                    ),
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {},
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: context.colors.createhomeitem,
-                            child: SvgPicture.asset(
-                              context.icons.taskstar,
+                      ),
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: context.colors.createhomeitem,
+                              child: SvgPicture.asset(AppIcons.expense),
                             ),
-                          ),
-                          const Gap(8),
-                          DefaultTextStyle(
-                            style: const TextStyle(
-                                decoration: TextDecoration.none),
-                            child: Text(context.localization.event,
+                            const Gap(8),
+                            Text(context.localization.expense,
                                 style:
                                     context.style.fontSize14Weight500.copyWith(
                                   color: context.colors.createhomeitemwhite,
                                 )),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: context.colors.createhomeitem,
+                              child: SvgPicture.asset(context.icons.taskhome),
+                            ),
+                            const Gap(8),
+                            Text(context.localization.tasks,
+                                style:
+                                    context.style.fontSize14Weight500.copyWith(
+                                  color: context.colors.createhomeitemwhite,
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const Gap(61),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: context.colors.createhomeitem,
+                              child: SvgPicture.asset(context.icons.note),
+                            ),
+                            const Gap(8),
+                            Text(context.localization.notes,
+                                style:
+                                    context.style.fontSize14Weight500.copyWith(
+                                  color: context.colors.createhomeitemwhite,
+                                )),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor:
+                                context.colors.createhomeitem.withOpacity(0.0),
                           ),
+                          const Gap(8),
+                          Text("",
+                              style: context.style.fontSize14Weight500.copyWith(
+                                color: context.colors.createhomeitemwhite,
+                              )),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: SvgPicture.asset(context.icons.closecreate),
-                ),
-              ],
-            ),
-            const Gap(28),
-          ],
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: context.colors.createhomeitem,
+                              child: SvgPicture.asset(
+                                context.icons.taskstar,
+                              ),
+                            ),
+                            const Gap(8),
+                            Text(context.localization.event,
+                                style:
+                                    context.style.fontSize14Weight500.copyWith(
+                                  color: context.colors.createhomeitemwhite,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: SvgPicture.asset(context.icons.closecreate),
+                  ),
+                ],
+              ),
+              const Gap(28),
+            ],
+          ),
         ),
       ),
     );
